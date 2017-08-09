b666f75946aa8692bbec880f9d4733bbae08f9d9
KEVINHSU-M83\KevinHsu <kevin860612@gmail.com>
2017-08-09 16:48:59 +0800

    background_layout

--- estore/views.py
+++ estore/views.py
@@ -5,13 +5,22 @@ from django.views import generic
 
 from .models import Product
 
+
 # Create your views here.
 
-class ProductList(generic.ListView):
+class ProductList(PermissionRequiredMixin, generic.ListView):
     model = Product
+
+    def has_permission(self):
+        if self.permission_required:
+            return super(ProductList, self).has_permission()
+        else:
+            return True
+
 
 class ProductDetail(generic.DetailView):
     model = Product
+
 
 class ProductCreate(PermissionRequiredMixin, generic.CreateView):
     permission_required = 'estore.add_product'
@@ -20,7 +29,8 @@ class ProductCreate(PermissionRequiredMixin, generic.CreateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已新增')
-        return reverse('product_list')
+        return reverse('dashboard_product_list')
+
 
 class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
     permission_required = 'estore.change_product'
@@ -29,4 +39,4 @@ class ProductUpdate(PermissionRequiredMixin, generic.UpdateView):
 
     def get_success_url(self):
         messages.success(self.request, '產品已變更')
-        return reverse('product_update', kwargs=self.kwargs)
+        return reverse('dashboard_product_update', kwargs=self.kwargs)
