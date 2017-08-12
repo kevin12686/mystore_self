de7d8c7fe6696b74897b8fb33f2663e325cfb2b5
KEVINHSU-M83\KevinHsu <kevin860612@gmail.com>
2017-08-12 16:15:05 +0800

    add check_out

--- estore/urls.py
+++ estore/urls.py
@@ -10,18 +10,16 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)/addtocart$', views.ProductAddToCart.as_view(), name='product_addtocart'),
 
     url(r'^cart/$', views.CartDetailFromRequest.as_view(), name='cart_detail'),
+    url(r'^cart/checkout$', views.OrderCreateCartCheckout.as_view(), name='cart_checkout'),
 
-    url(r'^dashboard/products$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html',
-                                                           permission_required='estore.change_product'),
-        name='dashboard_product_list'),
+    url(r'^dashboard/products$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html',permission_required='estore.change_product'),name='dashboard_product_list'),
     url(r'^dashboard/products/create$', views.ProductCreate.as_view(), name='dashboard_product_create'),
     url(r'^dashboard/products/(?P<pk>\d+)/update$', views.ProductUpdate.as_view(), name='dashboard_product_update'),
     url(r'^dashboard/products/(?P<pk>\d+)/delete$', views.ProductDelete.as_view(), name='dashboard_product_delete'),
 
     url(r'^dashboard/users/$', views.UserList.as_view(), name='dashboard_user_list'),
     url(r'^dashboard/users/(?P<pk>\d+)/addtostaff$', views.UserAddToStaff.as_view(), name='dashboard_user_addtostaff'),
-    url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(),
-        name='dashboard_user_removefromstaff'),
+    url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(),name='dashboard_user_removefromstaff'),
 ]
 
 if settings.DEBUG:
