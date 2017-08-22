864fb1c15a957a3cb9a96a080cf634f8603cd4bc
KEVINHSU-M83\KevinHsu <kevin860612@gmail.com>
2017-08-22 13:30:41 +0800

    delete a  product from cart

--- estore/urls.py
+++ estore/urls.py
@@ -12,15 +12,16 @@ urlpatterns = [
     url(r'^cart/$', views.CartDetailFromRequest.as_view(), name='cart_detail'),
     url(r'^cart/checkout$', views.OrderCreateCartCheckout.as_view(), name='cart_checkout'),
     url(r'^cart/clear$', views.CartDelete.as_view(), name='cart_delete'),
+    url(r'^cart/(?P<pk>\d+)/remove', views.CartItemDelete.as_view(), name='cart_item_delete'),
 
-    url(r'^dashboard/products$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html',permission_required='estore.change_product'),name='dashboard_product_list'),
+    url(r'^dashboard/products$', views.ProductList.as_view(template_name='estore/dashboard_product_list.html', permission_required='estore.change_product'),name='dashboard_product_list'),
     url(r'^dashboard/products/create$', views.ProductCreate.as_view(), name='dashboard_product_create'),
     url(r'^dashboard/products/(?P<pk>\d+)/update$', views.ProductUpdate.as_view(), name='dashboard_product_update'),
     url(r'^dashboard/products/(?P<pk>\d+)/delete$', views.ProductDelete.as_view(), name='dashboard_product_delete'),
 
     url(r'^dashboard/users/$', views.UserList.as_view(), name='dashboard_user_list'),
     url(r'^dashboard/users/(?P<pk>\d+)/addtostaff$', views.UserAddToStaff.as_view(), name='dashboard_user_addtostaff'),
-    url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(),name='dashboard_user_removefromstaff'),
+    url(r'^dashboard/users/(?P<pk>\d+)/removefromstaff$', views.UserRemoveFromStaff.as_view(), name='dashboard_user_removefromstaff'),
 
     url(r'^order/(?P<token>[0-9a-f-]+)/$', views.OrderDetail.as_view(), name='order_detail'),
     url(r'^order/(?P<token>[0-9a-f-]+)/pay_with_credit_card$', views.OrderPayWithCreditCard.as_view(),
